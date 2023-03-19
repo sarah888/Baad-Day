8bb517d6f88457fb177f1c7b1e9dda83584c1d63
sarah888 <shuan392@uwo.ca>
2023-03-19 09:36:12 -0400

    added radio buttons

--- level3/level3.js
+++ level3/level3.js
@@ -1,3 +1,9 @@
+let affirmation = document.getElementById("checkbox1");
+let hospital = document.getElementById("checkbox2");
+
+const confirm = document.getElementById("confirm");
+
+
 const cohere = require('cohere-ai'); // This is your trial API key
 cohere.init('yhlIG1WYyeUVrAJ2NzhQwYKnghq6sbs8DfWCPulm');
 
@@ -10,3 +16,34 @@ var o = (async () => {
   console.log(`The confidence levels of the labels are ${JSON.stringify(response.body.classifications)}`);
 })();
 
+
+affirmation.addEventListener("click", ()=>{
+  hospital.checked = false
+})
+
+hospital.addEventListener("click", ()=>{
+  affirmation.checked = false
+  alert("word")
+})
+
+
+confirm.addEventListener('click', () => {
+  if (confirm.innerHTML=="GO TO LUNCH"){
+      next.href="../level2/level2.html"
+  }
+  confirm.innerHTML="GO TO LUNCH";
+  position.innerHTML=""
+  
+  if (checkBox.checked){
+    text1.innerHTML = "Although this is an intriguing option, it is important to "
+    text2.innerHTML = "understand that sleeping in is a short-term pleasure and "
+    text3.innerHTML = "will not benefit Sheeper in the long-term, especially if "
+    text4.innerHTML = "they are truly passionate about being an active member "
+    text5.innerHTML = "on student council."
+  }else{
+    text1.innerHTML = "Right on the money! Sleeping in can decrease productivity and"
+    text2.innerHTML = "increase the risk of developing depression or other mood"
+    text3.innerHTML = "disorders. Now, Sheeper can successfully prepare and "
+    text4.innerHTML = "ace his interview!"
+  }
+});
