e4f87934c434cfbd16032a4255e1e2cfffca93f7
Claire Zhang <czhan828@uwo.ca>
2023-03-19 10:46:54 -0400

    blank

--- level3/level3.js
+++ level3/level3.js
@@ -1,72 +1,51 @@
 let affirmation = document.getElementById("checkbox1");
 let hospital = document.getElementById("checkbox2");
+let affirmation1 = document.getElementById("affirmation1");
 
 const confirm = document.getElementById("confirm");
 
-var o = "";
-
 confirm.addEventListener('click', buttonActivate);
 function buttonActivate(){
+  alert(pog);
   if (confirm.innerHTML=="GO TO BED"){
     next.href="../level4/level4.html";
   }
-  
+
   confirm.innerHTML="GO TO BED";
 }
 
-
-
-// const settings = {
-//   async: true,
-//   crossDomain: true,
-//   url: 'https://api.cohere.ai/v1/classify',
-//   method: 'POST',
-//   headers: {
-//     accept: 'application/json',
-//     'content-type': 'application/json',
-//     authorization: 'Bearer yhlIG1WYyeUVrAJ2NzhQwYKnghq6sbs8DfWCPulm'
-//   },
-//   processData: false,
-//   data: '{"inputs":["Confirm your email address","hey i need u to send some $"],"examples":[{"text":"Dermatologists don\'t like her!","label":"Spam"},{"text":"Hello, open to this?","label":"Spam"},{"text":"I need help please wire me $1000 right now","label":"Spam"},{"text":"Nice to know you ;)","label":"Spam"},{"text":"Please help me?","label":"Spam"},{"text":"Your parcel will be delivered today","label":"Not spam"},{"text":"Review changes to our Terms and Conditions","label":"Not spam"},{"text":"Weekly sync notes","label":"Not spam"},{"text":"Re: Follow up from today’s meeting","label":"Not spam"},{"text":"Pre-read for tomorrow","label":"Not spam"}],"truncate":"END"}'
-// };
-
-// $.ajax(settings).done(function (response) {
-//   alert(response);
-// });
-
-
 affirmation.addEventListener("click", ()=>{
   hospital.checked = false
-})
+});
 
 hospital.addEventListener("click", ()=>{
   affirmation.checked = false
   alert("word")
-})
-
-
-confirm.addEventListener('click', () => {
-  if (confirm.innerHTML=="GO TO BED"){
-      next.href="../level4/level4.html"
-  }
-  confirm.innerHTML="GO TO BED";
-  position.innerHTML=""
-  
-  if (affirmation.checked){
-    if(o.localeCompare("Uplifting") == 0){
-      // cohere return is good
-      text1.innerHTML = "You hit the nail on the head! You recognized the severity of"
-      text2.innerHTML = "Sheeper's condition and acted accordingly."
-    }else{
-      //cohere return is bad
-      text1.innerHTML = "Good thinking, you recognized the severity of Sheeper's"
-      text2.innerHTML = "condition. Try a gentler delivery next time."
-    }
-
-
-  }else{
-    text1.innerHTML = "Unfortunately, hospitals are at 100% capacity and there is "
-    text2.innerHTML = "no one to tend to Sheeper. They had to go home."
-
-  }
 });
+
+
+// confirm.addEventListener('click', () => {
+//   if (confirm.innerHTML=="GO TO BED"){
+//       next.href="../level4/level4.html"
+//   }
+//   confirm.innerHTML="GO TO BED";
+//   position.innerHTML=""
+  
+//   if (affirmation.checked){
+//     if(o.localeCompare("Uplifting") == 0){
+//       // cohere return is good
+//       text1.innerHTML = "You hit the nail on the head! You recognized the severity of"
+//       text2.innerHTML = "Sheeper's condition and acted accordingly."
+//     }else{
+//       //cohere return is bad
+//       text1.innerHTML = "Good thinking, you recognized the severity of Sheeper's"
+//       text2.innerHTML = "condition. Try a gentler delivery next time."
+//     }
+
+
+//   }else{
+//     text1.innerHTML = "Unfortunately, hospitals are at 100% capacity and there is "
+//     text2.innerHTML = "no one to tend to Sheeper. They had to go home."
+
+//   }
+// });
