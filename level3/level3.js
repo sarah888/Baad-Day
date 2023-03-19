753c23462710e68da377d0db3ef48a6b60436467
sarah888 <shuan392@uwo.ca>
2023-03-19 10:51:15 -0400

    level3 DONE!

--- level3/level3.js
+++ level3/level3.js
@@ -2,17 +2,19 @@ let affirmation = document.getElementById("checkbox1");
 let hospital = document.getElementById("checkbox2");
 
 const confirm = document.getElementById("confirm");
+const next = document.getElementById("next");
 
-var o = "";
 
-confirm.addEventListener('click', buttonActivate);
-function buttonActivate(){
-  if (confirm.innerHTML=="GO TO BED"){
-    next.href="../level4/level4.html";
-  }
+// var o = "";
+
+// confirm.addEventListener('click', buttonActivate);
+// function buttonActivate(){
+//   if (confirm.innerHTML=="GO TO BED"){
+//     next.href="../level4/level4.html";
+//   }
   
-  confirm.innerHTML="GO TO BED";
-}
+//   confirm.innerHTML="GO TO BED";
+// }
 
 
 
@@ -41,7 +43,6 @@ affirmation.addEventListener("click", ()=>{
 
 hospital.addEventListener("click", ()=>{
   affirmation.checked = false
-  alert("word")
 })
 
 
@@ -50,20 +51,18 @@ confirm.addEventListener('click', () => {
       next.href="../level4/level4.html"
   }
   confirm.innerHTML="GO TO BED";
-  position.innerHTML=""
   
   if (affirmation.checked){
-    if(o.localeCompare("Uplifting") == 0){
-      // cohere return is good
-      text1.innerHTML = "You hit the nail on the head! You recognized the severity of"
-      text2.innerHTML = "Sheeper's condition and acted accordingly."
-    }else{
+    // if(o.localeCompare("Uplifting") != 0){
+    //   alert("1")
+    //   // cohere return is good
+    //   text1.innerHTML = "You hit the nail on the head! You recognized the severity of"
+    //   text2.innerHTML = "Sheeper's condition and acted accordingly."
+    // }else{
       //cohere return is bad
-      text1.innerHTML = "Good thinking, you recognized the severity of Sheeper's"
-      text2.innerHTML = "condition. Try a gentler delivery next time."
-    }
-
-
+      text1.innerHTML = "Good thinking, you recognized the severity of Sheeper's condition."
+      text2.innerHTML = "Try a gentler delivery next time."
+    // }
   }else{
     text1.innerHTML = "Unfortunately, hospitals are at 100% capacity and there is "
     text2.innerHTML = "no one to tend to Sheeper. They had to go home."
