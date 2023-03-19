a577bf8573ff111003e297d5e43ee69e381ae6e3
Anson <aruan4@uwo.ca>
2023-03-18 21:50:45 -0400

    meowmeowmeow

--- level1/level1.js
+++ level1/level1.js
@@ -1,4 +1,5 @@
 const checkBox = document.getElementById("potato");
+const button = document.getElementById("poo");
 
 var decision = "";
 
@@ -11,3 +12,18 @@ function checkCheck(){
         decision = "Let him sleep in";
       }
 };
+
+button.addEventListener('click', () => {
+    if (button.innerHTML=="GO PACK LUNCH"){
+        next.href="../end/end.html"
+    }
+    validate1();
+    validate2();
+    validate3();
+    validate4();
+    validate5();
+    score = good/bad;
+    confirm.innerHTML="GO PACK LUNCH";
+    alert(score);
+
+});
