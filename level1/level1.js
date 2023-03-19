8ad1bfe4c5929a6ef897b7984fafec005a433558
Anson <aruan4@uwo.ca>
2023-03-18 21:34:33 -0400

    poopie

--- level1/level1.js
+++ level1/level1.js
@@ -1,11 +1,13 @@
 const checkBox = document.getElementById("potato");
 
+var decision = "";
+
 checkBox.addEventListener('change', checkCheck);
 
 function checkCheck(){
     if (this.checked) {
-        alert("Checkbox is checked..");
+        decision = "Gently wake him up";
       } else {
-        alert("Checkbox is not checked..");
+        decision = "Let him sleep in";
       }
 };
