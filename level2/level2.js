66cdf075760b8fb5b4b828eaf89c6fbf6e957860
laur-liu <lliu644@uwo.ca>
2023-03-18 20:19:18 -0400

    implemented API!

---
+++ level2/level2.js
@@ -0,0 +1,29 @@
+const apiKey = "ZRf2ijG6bg0QNbQoKTAIk77ECypW8XMqgNJgdJnf";
+
+// Get the user input of ingredients
+const inputIngredients = prompt("Enter a comma-separated list of ingredients:");
+
+// Split the input string into an array of ingredients
+const ingredients = inputIngredients.split(",").map(ingredient => ingredient.trim());
+
+// API endpoint
+const url = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${apiKey}&query=${ingredients.join(",")}`;
+
+// Get request
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    // Extract the food items from the JSON data
+    const foods = data.foods;
+
+    // Loop through the food items and log their nutrient information
+    foods.forEach(food => {
+      console.log(food.description);
+      console.log("Nutrients:");
+      food.foodNutrients.forEach(nutrient => {
+        console.log(`${nutrient.nutrientName}: ${nutrient.value} ${nutrient.unitName}`);
+      });
+      console.log("----");
+    });
+  })
+  .catch(error => console.error(error));
